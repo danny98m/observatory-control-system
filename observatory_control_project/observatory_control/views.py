e9df16f0c32ce92453eaac940cccce0ebdf90a80
danny98m <dpmullen98@gmail.com>
2020-03-03 17:47:36 -0500

    Set up observatory control application

--- observatory_control_project/observatory_control/views.py
+++ observatory_control_project/observatory_control/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home (request):
-    return HttpResponse('<h1>Hello</h1>')
+    return render(request, 'observatory_control/index.html')
