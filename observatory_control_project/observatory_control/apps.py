9f21dac0553f3d6a6800d1dc5e197b878f943e11
danny98m <dpmullen98@gmail.com>
2020-04-05 19:39:45 -0400

    Stylize login page

--- observatory_control_project/observatory_control/apps.py
+++ observatory_control_project/observatory_control/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class ObservatoryControlConfig(AppConfig):
     name = 'observatory_control'
--- observatory_control_project/observatory_control_project/settings.py
+++ observatory_control_project/observatory_control_project/settings.py
@@ -128,3 +128,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'observatory-home'
 
 LOGIN_URL = "login"
+
+LOGOUT_REDIRECT_URL = "login"
