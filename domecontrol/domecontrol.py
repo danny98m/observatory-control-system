f3728a6814b90bccd34699dc874b12ed851560c8
Jack-Fredward <johnathan.prideaux@gmail.com>
2020-04-11 18:39:10 -0400

    prune unnecessary files and add dome controls

--- domecontrol/domecontrol.py
+++ domecontrol/domecontrol.py
@@ -1,17 +1,54 @@
-#!/usr/bin/env python3
-""" Dome Controller
+""" Dome Control library
 
-Opens and closes Astrohaven domes.
-
-This Python script relies on Tkinter to provide a GUI.  If it's not installed on
-your system, you're in trouble.
+Python functions that will open and close astrohaven domes.
 """
 
-import domecontrolui
 import astrohaven
-from tkinter import Tk
+import serialist
+from forecastiopy import *
 
 
-gui = domecontrolui.DomeList(master=Tk())
-gui.master.title("Dome Control")
-gui.mainloop()
+class dome_control:
+    """A single dome to be controlled."""
+    def __init__(self):
+        self.dome = ''
+        self.connect()
+
+
+    def connect(self):
+        """Locate and connect to a dome."""
+        serlist = serialist.Serialist()
+        for port in serlist:
+            self.dome = astrohaven.Astrohaven(port)
+            if(not self.dome.ready):
+                self.dome.closeconn()
+
+
+    def disconnect(self):
+        """Disconnect from the dome."""
+        self.dome.closeconn()
+
+    def a_open(self):
+        """Open the A side of the dome."""
+        self.dome.fullmove('A','Open')
+
+    def b_open(self):
+        """Open the B side of the dome."""
+        self.dome.fullmove('B','Open')
+
+    def a_close(self):
+        """Close the A side of the dome."""
+        self.dome.fullmove('A','Close')
+
+    def b_close(self):
+        """Close the B side of the dome."""
+        self.dome.fullmove('B','Close')
+
+    def full_close(self):
+        """Close both sides of the dome."""
+        self.dome.fullclose()
+
+    def full_open(self):
+        """Open both sides of the dome."""
+        self.dome.fullopen()
+        
